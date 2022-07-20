9a181903916a4198ec9a6edd7d7b415bcc82e796
Kevin Nowak <kevin_nowak@epam.com>
2022-07-20 13:28:54 +0200

    Added BunnyEars2

---
+++ src/test/java/com/github/budison/codingbat/recursion1/BunnyEars2Test.java
@@ -0,0 +1,25 @@
+package com.github.budison.codingbat.recursion1;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class BunnyEars2Test {
+    @Test
+    void testBunnyEars2() {
+        // Given
+        SoftAssert softAssert = new SoftAssert();
+
+        // When
+        softAssert.assertEquals(BunnyEars2.bunnyEars2(0), 0);
+        softAssert.assertEquals(BunnyEars2.bunnyEars2(1), 2);
+        softAssert.assertEquals(BunnyEars2.bunnyEars2(2), 5);
+        softAssert.assertEquals(BunnyEars2.bunnyEars2(3), 7);
+        softAssert.assertEquals(BunnyEars2.bunnyEars2(4), 10);
+        softAssert.assertEquals(BunnyEars2.bunnyEars2(5), 12);
+        softAssert.assertEquals(BunnyEars2.bunnyEars2(6), 15);
+        softAssert.assertEquals(BunnyEars2.bunnyEars2(10), 25);
+
+        // Then
+        softAssert.assertAll();
+    }
+}
