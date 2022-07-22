4647ae294bcfc35125054e0b7e47dde2bdc594a0
Kevin Nowak <kevin_nowak@epam.com>
2022-07-22 10:15:41 +0200

    Added Count7

---
+++ src/test/java/com/github/budison/codingbat/recursion1/Count7Test.java
@@ -0,0 +1,26 @@
+package com.github.budison.codingbat.recursion1;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class Count7Test {
+    @Test
+    void testCount7() {
+        // Given
+        SoftAssert softAssert = new SoftAssert();
+
+        // When
+        softAssert.assertEquals(Count7.count7(0), 0);
+        softAssert.assertEquals(Count7.count7(7), 1);
+        softAssert.assertEquals(Count7.count7(4), 0);
+        softAssert.assertEquals(Count7.count7(17), 1);
+        softAssert.assertEquals(Count7.count7(23), 0);
+        softAssert.assertEquals(Count7.count7(77), 2);
+        softAssert.assertEquals(Count7.count7(770), 2);
+        softAssert.assertEquals(Count7.count7(1234), 0);
+        softAssert.assertEquals(Count7.count7(7172737), 4);
+
+        // Then
+        softAssert.assertAll();
+    }
+}
