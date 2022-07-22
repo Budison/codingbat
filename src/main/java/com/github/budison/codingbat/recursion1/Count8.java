33b3bf53094283541dabef314a4ab072717737ca
Kevin Nowak <kevin_nowak@epam.com>
2022-07-22 10:30:15 +0200

    Added Count8

---
+++ src/main/java/com/github/budison/codingbat/recursion1/Count8.java
@@ -0,0 +1,17 @@
+package com.github.budison.codingbat.recursion1;
+
+abstract class Count8 {
+    static int count8(int n) {
+        int count = 0;
+
+        if (n % 10 == 8) {
+            if (n / 10 % 10 == 8) {
+                count = 2;
+            } else {
+                count = 1;
+            }
+        }
+
+        return n / 10 == 0 ? count : count8(n / 10) + count;
+    }
+}
---
+++ src/test/java/com/github/budison/codingbat/recursion1/Count8Test.java
@@ -0,0 +1,25 @@
+package com.github.budison.codingbat.recursion1;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class Count8Test {
+    @Test
+    void testCount8() {
+        // Given
+        SoftAssert softAssert = new SoftAssert();
+
+        // When
+        softAssert.assertEquals(Count8.count8(8), 1);
+        softAssert.assertEquals(Count8.count8(818), 2);
+        softAssert.assertEquals(Count8.count8(8818), 4);
+        softAssert.assertEquals(Count8.count8(8088), 4);
+        softAssert.assertEquals(Count8.count8(123), 0);
+        softAssert.assertEquals(Count8.count8(88888), 9);
+        softAssert.assertEquals(Count8.count8(77778), 1);
+        softAssert.assertEquals(Count8.count8(777788), 3);
+
+        // Then
+        softAssert.assertAll();
+    }
+}
