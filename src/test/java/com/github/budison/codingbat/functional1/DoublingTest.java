b107484f62ddaf4180efd7854afdd6adddfb5544
Kevin Nowak <nowakk96@icloud.com>
2022-08-24 11:50:39 +0200

    test: update DoublingTest

--- src/test/java/com/github/budison/codingbat/functional1/DoublingTest.java
+++ src/test/java/com/github/budison/codingbat/functional1/DoublingTest.java
@@ -13,17 +13,17 @@ public class DoublingTest {
     @DataProvider
     public Object[][] testDoublingProvider() {
         return new Object[][]{
-                {new ArrayList<>(List.of(1, 2, 3)), new ArrayList<>(List.of(2, 4, 6))},
-                {new ArrayList<>(List.of(6, 8, 6, 8, -1)), new ArrayList<>(List.of(12, 16, 12, 16, -2))},
-                {new ArrayList<>(List.of()), new ArrayList<>(List.of())},
-                {new ArrayList<>(List.of(5)), new ArrayList<>(List.of(10))},
-                {new ArrayList<>(List.of(5, 10)), new ArrayList<>(List.of(10, 20))},
-                {new ArrayList<>(List.of(8, -5, 7, 3, 109)), new ArrayList<>(List.of(16, -10, 14, 6, 218))}
+                {List.of(1, 2, 3), List.of(2, 4, 6)},
+                {List.of(6, 8, 6, 8, -1), List.of(12, 16, 12, 16, -2)},
+                {List.of(), List.of()},
+                {List.of(5), List.of(10)},
+                {List.of(5, 10), List.of(10, 20)},
+                {List.of(8, -5, 7, 3, 109), List.of(16, -10, 14, 6, 218)}
         };
     }
 
     @Test(dataProvider = "testDoublingProvider")
-    void testDoubling(ArrayList<Integer> input, ArrayList<Integer> expected) {
-        assertEquals(Doubling.doubling(input), expected);
+    void testDoubling(List<Integer> input, List<Integer> expected) {
+        assertEquals(Doubling.doubling(new ArrayList<>(input)), expected);
     }
 }
